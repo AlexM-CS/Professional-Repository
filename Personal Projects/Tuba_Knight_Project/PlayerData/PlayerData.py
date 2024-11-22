b1f0e90e6eb7ba57c86e071b4eb97ce5954a8cbc
AlexM-CS <amyska04@iastate.edu>
2024-11-22 12:37:41 -0600

    Added the beginnings of the Tuba Knight Project

---
+++ Personal Projects/Tuba_Knight_Project/PlayerData/PlayerData.py
@@ -0,0 +1,16 @@
+# Created: 11-22-2024
+# Last updated: 11-22-2024
+
+# IO packages:
+
+# Graphics packages:
+
+# Built-in packages
+
+# External packages:
+
+# Internal packages:
+from ..tubaKnight.Entities.Characters.Stats import Stats
+
+# The PlayerData class is what player-characters use to hold all
+# their relevant data, such as Stats, Items, Level, etc.
---
+++ Personal Projects/Tuba_Knight_Project/tubaKnight/__init__.py
@@ -0,0 +1,12 @@
+# Created: 11-21-2024
+# Last updated: 11-21-2024
+
+# IO packages:
+from . import IO
+
+# Graphics packages:
+from . import Graphics
+
+# Built-in packages:
+
+# External packages:
